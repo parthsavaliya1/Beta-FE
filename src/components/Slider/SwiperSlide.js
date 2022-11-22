ed8c7c929834242e1d7e801cd28659cfafb42826
Lavesh Katariya <lavesh.k@brilworks.com>
2022-11-22 10:11:04 +0530

    slider implement

---
+++ src/components/Slider/SwiperSlide.js
@@ -0,0 +1,36 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "./swiper.css";
+
+const SwiperSlider = () => {
+  return (
+    <Swiper
+      modules={[Pagination, Scrollbar, A11y]}
+      spaceBetween={20}
+      slidesPerView={1.1}
+      centeredSlides={true}
+      initialSlide={1}
+      roundLengths={true}
+      loopAdditionalSlides={30}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
+
+export default SwiperSlider;
